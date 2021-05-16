f39d87893e162024730d7a43571a0de2d80f9f33
Amanda <amanda.tavares@ubistart.com>
2021-05-16 18:53:21 -0300

    Initial Commit

---
+++ programa-n-versao/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  programa-n-versao
+//
+//  Created by Amanda Tavares on 16/05/21.
+//  Copyright © 2021 Amanda Tavares. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
